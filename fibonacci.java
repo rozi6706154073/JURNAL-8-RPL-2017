72b5ff3247d279b3e70d28b84e5018d2f486aab8
Rozi Wardana <mrozin22@gmail.com>
2017-04-05 09:10:40 +0700

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+public class Fibonacci{
+
+    
+       
+    private static void Fib(int n){
+        int p,q,r,i;
+        p=0;
+        q=1;
+        for(i=1;i<=n;i++){
+            System.out.print("  "+p);
+            r=p+q;
+            p=q;
+            q=r;
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Maukkan N: \t");
+        int N=input.nextInt();
+        if (N>=1){
+             Fib(N);
+        }
+        else{
+            System.out.println("N harus lebih besar dari 1                                  !!!");
+        }
+    }
+    
+}
+     
+
